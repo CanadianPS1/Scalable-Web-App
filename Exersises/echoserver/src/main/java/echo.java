d876a1bc3a5762e5aa4f2cb92b2ed12e6d8427d4
Lia <sethroeback1@gmail.com>
2025-10-10 13:32:15 -0600

    yes

--- Exersises/echoserver/src/main/java/echo.java
+++ Exersises/echoserver/src/main/java/echo.java
@@ -8,9 +8,19 @@ import jakarta.servlet.http.HttpServletResponse;
  
 @WebServlet(name = "echo", value = "/echo")
 public class echo extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-        throws ServletException, IOException{
-            response.getWriter().println("Welcome to my Echo");
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.getWriter().println("Welcome to my Echo");
+        //for the get
+        String x = request.getParameter("x");
+        String y = request.getParameter("y");
+        response.getWriter().println(x + " my get concatination of x and y " + y);        
     }
-   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        //for the post
+        String x = request.getParameter("x");
+        String y = request.getParameter("y");
+        response.getWriter().println(x + " my get concatination of x and y " + y);        
+    }
 }
